8faf50440fdc3ae8bfe98d92bc924b318c9c0e85
scholarly-punk <169328705+scholarly-punk@users.noreply.github.com>
2024-09-04 22:19:54 +1000

    Wrote real and integer predicates

--- src/main.lisp
+++ src/main.lisp
@@ -1,5 +1,33 @@
 (in-package :scholarly-math)
 
-(defun positive-integer-p (x)
-  "Is x a positive-integer?"
-  (and (integerp x) (plusp x)))
+(defun non-minus-p (number)
+  "Is this real number not negative?"
+  (not (minusp number)))
+
+(defun non-plus-p (number)
+  "Is this real number not positive?"
+  (not (plusp number)))
+
+(defun non-zero-p (number)
+  "Is this real number not zero?"
+  (not (zerop number)))
+
+(defun plus-integer-p (object)
+  "Return true if OBJECT is a positive INTEGER, and NIL otherwise."
+  (and (integerp object) (plusp object)))
+
+(defun minus-integer-p (object)
+  "Return true if OBJECT is a negative INTEGER, and NIL otherwise."
+  (and (integerp object) (minusp object)))
+
+(defun non-minus-integer-p (object)
+  "Return true if OBJECT is a non negative INTEGER, and NIL otherwise."
+  (and (integerp object) (non-minus-p object)))
+
+(defun non-plus-integer-p (object)
+  "Return true if OBJECT is a non positive INTEGER, and NIL otherwise."
+  (and (integerp object) (non-plus-p object)))
+
+(defun non-zero-integer-p (object)
+  "Return true if OBJECT is a non zero INTEGER, and NIL otherwise."
+  (and (integerp object) (non-zero-p object)))
--- src/package.lisp
+++ src/package.lisp
@@ -1,5 +1,12 @@
 (defpackage :scholarly-math
   (:use :cl)
-  (:export #:positive-integer-p))
+  (:export #:non-minus-p
+	   #:non-plus-p
+	   #:non-zero-p
+	   #:plus-integer-p
+	   #:minus-integer-p
+	   #:non-minus-integer-p
+	   #:non-plus-integer-p
+	   #:non-zero-integer-p))
 
 (in-package :scholarly-math)
