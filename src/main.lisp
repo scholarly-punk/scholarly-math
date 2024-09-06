097dff758bd60eae72a14fbe2294d89d27eb238c
scholarly-punk <169328705+scholarly-punk@users.noreply.github.com>
2024-09-06 16:15:55 +1000

    Created deftypes and updated predicates to use them.

--- src/main.lisp
+++ src/main.lisp
@@ -1,33 +1,73 @@
 (in-package :scholarly-math)
 
+(deftype plus-real ()
+  "A positive real."
+  '(real (0) *))
+
+(deftype minus-real ()
+  "A negative real."
+  '(real * (0)))
+
+(deftype non-minus-real ()
+  "A non-negative real."
+  '(real 0 *))
+
+(deftype non-plus-real ()
+  "A non-positive real."
+  '(real * 0))
+
+(deftype non-zero-real ()
+  "A non-zero real."
+  '(or (real * (0)) (real (0) *)))
+
+(deftype plus-integer ()
+  "A positive integer."
+  '(integer 1 *))
+
+(deftype minus-integer ()
+  "A negative integer."
+  '(integer * -1))
+
+(deftype non-minus-integer ()
+  "A non-negative integer."
+  '(integer 0 *))
+
+(deftype non-plus-integer ()
+  "A non-positive integer."
+  '(integer * 0))
+
+(deftype non-zero-integer ()
+  "A non-zero integer."
+  '(or (integer * -1) (integer 1 *) ))
+
 (defun non-minus-p (number)
   "Is this real number not negative?"
-  (not (minusp number)))
+  (typep number 'non-minus-real))
 
 (defun non-plus-p (number)
   "Is this real number not positive?"
-  (not (plusp number)))
+  (typep number 'non-plus-real))
 
 (defun non-zero-p (number)
   "Is this real number not zero?"
-  (not (zerop number)))
+  (typep number 'non-zero-real))
 
 (defun plus-integer-p (object)
   "Return true if OBJECT is a positive INTEGER, and NIL otherwise."
-  (and (integerp object) (plusp object)))
+  (typep object 'plus-integer))
 
 (defun minus-integer-p (object)
   "Return true if OBJECT is a negative INTEGER, and NIL otherwise."
-  (and (integerp object) (minusp object)))
+  (typep object 'minus-integer))
 
 (defun non-minus-integer-p (object)
   "Return true if OBJECT is a non negative INTEGER, and NIL otherwise."
-  (and (integerp object) (non-minus-p object)))
+  (typep object 'non-minus-integer))
 
 (defun non-plus-integer-p (object)
   "Return true if OBJECT is a non positive INTEGER, and NIL otherwise."
-  (and (integerp object) (non-plus-p object)))
+  (typep object 'non-plus-integer))
 
 (defun non-zero-integer-p (object)
   "Return true if OBJECT is a non zero INTEGER, and NIL otherwise."
-  (and (integerp object) (non-zero-p object)))
+  (typep object 'non-zero-integer))
--- src/package.lisp
+++ src/package.lisp
@@ -7,6 +7,16 @@
 	   #:minus-integer-p
 	   #:non-minus-integer-p
 	   #:non-plus-integer-p
-	   #:non-zero-integer-p))
+	   #:non-zero-integer-p
+	   #:plus-integer
+	   #:minus-integer
+	   #:non-minus-integer
+	   #:non-plus-integer
+	   #:non-zero-integer
+	   #:plus-real
+	   #:minus-real
+	   #:non-minus-real
+	   #:non-plus-real
+	   #:non-zero-real))
 
 (in-package :scholarly-math)
