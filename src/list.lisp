961b1de7c7593082a245eb82bd53e17c53a168e9
scholarly-punk <169328705+scholarly-punk@users.noreply.github.com>
2024-10-06 16:53:31 +1100

    Added type checking

--- src/list.lisp
+++ src/list.lisp
@@ -1,6 +1,10 @@
+(in-package :scholarly-math)
+
 (defun build-list (n fn)
   "Creates a list of N elements by applying function FN to the integers
 from 0 to (1- N)."
+  (check-type n plus-integer)
+  (check-type fn function)
   (do ((i (1- n) (1- i))
        (list '() (cons (funcall fn i) list)))
       ((< i 0) list)))
