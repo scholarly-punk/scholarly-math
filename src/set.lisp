c95e9efc2851a8b8ba16e6d380a2130339f26ea4
scholarly-punk <169328705+scholarly-punk@users.noreply.github.com>
2024-11-30 17:56:06 +1100

    error detection and refinement

--- src/set.lisp
+++ src/set.lisp
@@ -1,6 +1,9 @@
 (in-package :scholarly-math)
 
-(defun set-equal (s1 s2)
+(defun set-equal (s1 s2 &key (test #'eql))
   "Return t if s1 is equal to s2. nil otherwise"
-  (and (subsetp s1 s2)
-       (subsetp s2 s1)))
+  (check-type s1 list)
+  (check-type s2 list)
+  (check-type test function)
+  (and (subsetp s1 s2 :test test)
+       (subsetp s2 s1 :test test)))
